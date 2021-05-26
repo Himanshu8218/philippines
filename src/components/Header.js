a41f320d947ad1103517bfe233fb4938a854d77d
Himanshu8218 <tyagihimanshu801@gmail.com>
2021-05-26 15:18:37 +0530

    logo video changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,7 +24,7 @@ export class Header extends Component {
                             height: '70vh',
                             overflow: 'hidden',
                         }}>
-                            <VideoCover videoOptions={{ src: `${process.env.PUBLIC_URL}/images/logo_animation.mp4`, autoPlay: true, muted: false }} />
+                            <VideoCover videoOptions={{ src: `${process.env.PUBLIC_URL}/images/logo-video.mp4`, autoPlay: true, muted: false }} />
                         </div>
                     </div>
                     <div>
